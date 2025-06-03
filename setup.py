f9105e40b6a5812678f13aac5ea1a3237c3ae861
William Warriner <6930772+wwarriner@users.noreply.github.com>
2025-06-03 15:34:16 -0500

    increment version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     keywords="mkdocs title case casing plugin",
     url="https://github.com/uabrc/mkdocs-title-casing-plugin",
     author="wwarriner",
-    version="1.0.0",
+    version="1.1.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
